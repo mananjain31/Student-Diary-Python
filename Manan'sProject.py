fbb46c026075979a969938025ff62e95cc7707d7
manan jain <60469574+mananjain31@users.noreply.github.com>
2020-04-29 16:21:08 +0530

    added database

--- Manan'sProject.py
+++ Manan'sProject.py
@@ -5,6 +5,7 @@ import sqlite3
 
 width,height="700","500"
 framecolor="green"
+framefg="white"
 fontsize=20
 
 
@@ -13,28 +14,58 @@ t.geometry(width+"x"+height)
 t.resizable(0,0)
 
 usern = StringVar ()
+usern2 = StringVar ()
 userp = StringVar ()
 usercn = StringVar ()
 
 
 def loggedin():
-    n=ttk.Notebook()
-    n.place(x=0,y=0,width=width,height=height)
+    notebookcolor="red"
+    nb=ttk.Notebook()
+    nb.place(x=0,y=0,width=width,height=height)
 
-    f1=Frame(bg="red")
-    n.add(f1,text="manan")
+
+    f1=Frame(bg=notebookcolor)
+    nb.add(f1,text="manan")
+    Label(f1, text="Enter Roll.No. ", bg=notebookcolor, font=("", 17)).place(x=60, y=50)
+    entryrn = Entry(f1, font=("", 15), width=18)
+    entryrn.place(x=300, y=50)
+    Label(f1, text="Enter Name", bg=notebookcolor, font=("", 17)).place(
+        x=60, y=100)
+    entryname = Entry(f1, font=("", 15), width=18)
+    entryname.place(x=300, y=100)
+    Label(f1, text="Physics Marks", bg=notebookcolor, font=("", 17)).place(
+        x=60, y=150)
+    entryphy = Entry(f1, font=("", 15), width=18)
+    entryphy.place(x=300, y=150)
+    Label(f1, text="Chemistry Marks", bg=notebookcolor, font=("", 17)).place(
+        x=60, y=200)
+    entrychem = Entry(f1, font=("", 15), width=18)
+    entrychem.place(x=300, y=200)
+    Label(f1, text="Mathematics Marks", bg=notebookcolor, font=("", 17)).place(
+        x=60, y=250)
+    entrymath = Entry(f1, font=("", 15), width=18)
+    entrymath.place(x=300, y=250)
+    Button(f1, text="Submit", font=("", 15), bg=notebookcolor,activebackground="blue"
+           ).place(x=int(width) / 3, y=int(height) / 2 + 100)
+
 
     f2=Frame(bg="green")
-    n.add(f2,text="manan")
+    nb.add(f2,text="manan")
+
+    f6=Frame(bg="black")
+    nb.add(f6,text="Logout")
+    Button(f6,text="logout",command=home).place(x=int(width)/2,y=int(height)/2)
 
 def login():
-    db = sqlite3.connect('users.db')
+    db = sqlite3.connect('projectdb.db')
     cr = db.cursor()
     got=cr.execute("select * from users where UNAME='"+usern.get()+"' AND UPASS='"+userp.get()+"'")
     for i in got :
         print(i)
         loggedin()
-        messagebox.showinfo("Its my First Python Project", "WELCOME "+usern.get()+" !!")
+        usern2.set(usern.get())
+        messagebox.showinfo("Its My First Python Project", "WELCOME "+usern2.get()+" !!")
         break
     else:
         messagebox.showinfo("Title","No such user found")
@@ -76,9 +107,9 @@ def testUserData():
 def submit():
     emsg=""
     if testUserData()==[True,True,True]:
-        db = sqlite3.connect('users.db')
+        db = sqlite3.connect('projectdb.db')
         cr = db.cursor()
-        #cr.execute("create table users(UNAME text , UPASS text , UCN text)")
+       # cr.execute("create table users(UNAME text , UPASS text , UCN text)")
         cr.execute(
             "insert into users(UNAME , UPASS , UCN) VALUES('"+usern.get()+"','"+userp.get()+"','"+usercn.get()+"')")
         db.commit()
@@ -152,7 +183,7 @@ def home():
     homepage=Frame(bg=framecolor)
     homepage.place(x=0,y=0,width=width,height=height)
 
-    Label(text="HOME PAGE",font=("",40)).place(x=int(width)/4,y=int(height)/10)
+    Label(text="HOME PAGE",font=("",40)).place(x=int(width)/4,y=int(height)/10)  
 
     blg=Button(homepage,text="Log in",font=("",20),width=10,height=1,
                command=loginPage)
@@ -163,4 +194,6 @@ def home():
     breg.place(x=int(width)/2+50,y=int(height)/2)
 
 home()
+
+
 t.mainloop()
