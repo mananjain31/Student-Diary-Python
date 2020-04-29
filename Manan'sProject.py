38008e33ffd25358e1568fd0eafb9c596d724727
manan jain <60469574+mananjain31@users.noreply.github.com>
2020-04-29 22:39:13 +0530

    improved last code

--- Manan'sProject.py
+++ Manan'sProject.py
@@ -25,117 +25,122 @@ phymarks = StringVar()
 chemarks = StringVar()
 mathmarks = StringVar()
 
-
+f33=None
 
 
 
 def loggedin(): 
     nb=ttk.Notebook()
     nb.place(x=0,y=0,width=width,height=height)
-
-
-    def insert(nb):
-        f1=Frame(bg=notebookcolor)
-        nb.add(f1,text="manan")
-        Label(f1, text="Enter Roll.No. ", bg=notebookcolor, font=("", 17)).place(x=60, y=50)
-        entryrn = Entry ( f1 , font = ( "" , 15 ) , width = 18 , textvariable = studentrno )
-        entryrn.place(x=300, y=50)
-        Label(f1, text="Enter Name", bg=notebookcolor, font=("", 17)).place(
-            x=60, y=100)
-        entryname = Entry(f1, font=("", 15), width=18 , textvariable = studentn)
-        entryname.place(x=300, y=100)
-        Label(f1, text="Physics Marks", bg=notebookcolor, font=("", 17)).place(
-            x=60, y=150)
-        entryphy = Entry(f1, font=("", 15), width=18 , textvariable = phymarks)
-        entryphy.place(x=300, y=150)
-        Label(f1, text="Chemistry Marks", bg=notebookcolor, font=("", 17)).place(
-            x=60, y=200)
-        entrychem = Entry(f1, font=("", 15), width=18 , textvariable = chemarks)
-        entrychem.place(x=300, y=200)
-        Label(f1, text="Mathematics Marks", bg=notebookcolor, font=("", 17)).place(
-            x=60, y=250)
-        entrymath = Entry(f1, font=("", 15), width=18 , textvariable = mathmarks)
-        entrymath.place(x=300, y=250)
-        def marksSubmit():
-            db=sqlite3.connect('projectdb.db')
-            cr=db.cursor()
-            cr.execute(
-                "insert into students(RNO,NAME,PHY,CHE,MATHS) VALUES('"+studentrno.get()+"','"+studentn.get()+"','"+phymarks.get()+"','"+chemarks.get()+"','"+mathmarks.get()+"')")
-            db.commit()
-            db.close()
-            print("success")
-            showall(nb)
-            studentrno.set("")
-            studentn.set("")
-            mathmarks.set("")
-            phymarks.set("")
-            chemarks.set("")
-        Button(f1, text="Submit", font=("", 15), bg=notebookcolor,activebackground="blue"
-               ,command=marksSubmit).place(x=int(width) / 3, y=int(height) / 2 + 100) 
-
-    def search(nb):
-        f2=Frame(bg = notebookcolor )
-        nb.add(f2,text="Search")
-        Label(f2,bg=notebookcolor).place(x=0,y=0,width=int(width),height=int(height))
-        Label(f2,text="Enter Roll. No.",bg=notebookcolor,fg="white",font=("",15)).place(x=int(width)/2-200,y=80)
-        Entry(f2,textvariable=studentrno,font=("",11),justify="right").place(x=int(width)/2-50,y=85,width=150) 
-        def searchdb():
-            pass
-        Button(f2,text="Search",bg=notebookcolor,fg="white",command=searchdb,
-            font=("",11)).place(x=int(width)/2+120,y=80,width=150)   
-
-
-    def showall(nb):
-        Label(f3,bg=notebookcolor).place(x=0,y=0,width=int(width),height=int(height))
-        Label(f3,text="Roll No.",bg=notebookcolor,fg="white",font=("",15)).place(x=30,y=10)
-        Label(f3,text="Name",bg=notebookcolor,fg="white",font=("",15)).place(x=180,y=10)
-        Label(f3,text="Physics ",bg=notebookcolor,fg="white",font=("",15)).place(x=300,y=10)
-        Label(f3,text="Chemistry",bg=notebookcolor,fg="white",font=("",15)).place(x=420,y=10)
-        Label(f3,text="Mathematics",bg=notebookcolor,fg="white",font=("",15)).place(x=540,y=10)
-        db=sqlite3.connect('projectdb.db')
-        cr=db.cursor()
-        r=cr.execute("select * from students")
-        x=30
-        y=40
-        for r1 in r:
-            if r1[0] =="" :
-                continue   
-            Label(f3,text=r1[0],font=("",11),bg=notebookcolor,fg="white").place(x=x,y=y)
-            x += 150
-            Label(f3,text=r1[1],font=("",11),bg=notebookcolor,fg="white").place(x=x,y=y)
-            x += 120
-            Label(f3,text=r1[2],font=("",11),bg=notebookcolor,fg="white").place(x=x,y=y)
-            x += 120
-            Label(f3,text=r1[3],font=("",11),bg=notebookcolor,fg="white").place(x=x,y=y)
-            x += 120
-            Label(f3,text=r1[4],font=("",11),bg=notebookcolor,fg="white").place(x=x,y=y)
-            x = 30
-            y+=20
-        db.commit()
-        db.close()
-
-    def update(nb):
-        f4=Frame(bg=notebookcolor  )
-        nb.add(f4,text="Update")
-
-    def delete(nb):
-        f5=Frame(bg=notebookcolor  )
-        nb.add(f5,text="Delete")
-
-
-    def logout(nb):
-        f6=Frame(bg= notebookcolor)
-        nb.add(f6,text="Logout")
-        Button(f6,text="logout",command=home).place(x=int(width)/2,y=int(height)/2)
-
     insert(nb)
-    search(nb)   
-    f3=Frame(bg=notebookcolor)
-    nb.add(f3,text="Show All")
-    showall(nb)
+    search(nb)
+    showa(nb)
     update(nb)
     delete(nb)
     logout(nb)
+
+def insert(nb):
+    f1=Frame(bg=notebookcolor)
+    nb.add(f1,text="manan")
+    Label(f1, text="Enter Roll.No. ", bg=notebookcolor, font=("", 17)).place(x=60, y=50)
+    entryrn = Entry ( f1 , font = ( "" , 15 ) , width = 18 , textvariable = studentrno )
+    entryrn.place(x=300, y=50)
+    Label(f1, text="Enter Name", bg=notebookcolor, font=("", 17)).place(
+        x=60, y=100)
+    entryname = Entry(f1, font=("", 15), width=18 , textvariable = studentn)
+    entryname.place(x=300, y=100)
+    Label(f1, text="Physics Marks", bg=notebookcolor, font=("", 17)).place(
+        x=60, y=150)
+    entryphy = Entry(f1, font=("", 15), width=18 , textvariable = phymarks)
+    entryphy.place(x=300, y=150)
+    Label(f1, text="Chemistry Marks", bg=notebookcolor, font=("", 17)).place(
+        x=60, y=200)
+    entrychem = Entry(f1, font=("", 15), width=18 , textvariable = chemarks)
+    entrychem.place(x=300, y=200)
+    Label(f1, text="Mathematics Marks", bg=notebookcolor, font=("", 17)).place(
+        x=60, y=250)
+    entrymath = Entry(f1, font=("", 15), width=18 , textvariable = mathmarks)
+    entrymath.place(x=300, y=250)
+    def marksSubmit():
+        global f33
+        db=sqlite3.connect('projectdb.db')
+        cr=db.cursor()
+        cr.execute(
+            "insert into students(RNO,NAME,PHY,CHE,MATHS) VALUES('"+studentrno.get()+"','"+studentn.get()+"','"+phymarks.get()+"','"+chemarks.get()+"','"+mathmarks.get()+"')")
+        db.commit()
+        db.close()
+        print("success")
+        showall(f33)
+        studentrno.set("")
+        studentn.set("")
+        mathmarks.set("")
+        phymarks.set("")
+        chemarks.set("")
+    Button(f1, text="Submit", font=("", 15), bg=notebookcolor,activebackground="blue"
+           ,command=marksSubmit).place(x=int(width) / 3, y=int(height) / 2 + 100) 
+
+def search(nb):
+    f2=Frame(bg = notebookcolor )
+    nb.add(f2,text="Search")
+    Label(f2,bg=notebookcolor).place(x=0,y=0,width=int(width),height=int(height))
+    Label(f2,text="Enter Roll. No.",bg=notebookcolor,fg="white",font=("",15)).place(x=int(width)/2-200,y=80)
+    Entry(f2,textvariable=studentrno,font=("",11),justify="right").place(x=int(width)/2-50,y=85,width=150) 
+    def searchdb():
+        pass
+    Button(f2,text="Search",bg=notebookcolor,fg="white",command=searchdb,
+        font=("",11)).place(x=int(width)/2+120,y=80,width=150)   
+
+def showa(nb):
+    global f33
+    f3=Frame(bg=notebookcolor)
+    nb.add(f3,text="Show All")
+    f33=f3
+    showall(f3)
+
+def showall(f3):
+    Label(f3,bg=notebookcolor).place(x=0,y=0,width=int(width),height=int(height))
+    Label(f3,text="Roll No.",bg=notebookcolor,fg="white",font=("",15)).place(x=30,y=10)
+    Label(f3,text="Name",bg=notebookcolor,fg="white",font=("",15)).place(x=180,y=10)
+    Label(f3,text="Physics ",bg=notebookcolor,fg="white",font=("",15)).place(x=300,y=10)
+    Label(f3,text="Chemistry",bg=notebookcolor,fg="white",font=("",15)).place(x=420,y=10)
+    Label(f3,text="Mathematics",bg=notebookcolor,fg="white",font=("",15)).place(x=540,y=10)
+    db=sqlite3.connect('projectdb.db')
+    cr=db.cursor()
+    r=cr.execute("select * from students")
+    x=30
+    y=40
+    for r1 in r:
+        if r1[0] =="" :
+            continue   
+        Label(f3,text=r1[0],font=("",11),bg=notebookcolor,fg="white").place(x=x,y=y)
+        x += 150
+        Label(f3,text=r1[1],font=("",11),bg=notebookcolor,fg="white").place(x=x,y=y)
+        x += 120
+        Label(f3,text=r1[2],font=("",11),bg=notebookcolor,fg="white").place(x=x,y=y)
+        x += 120
+        Label(f3,text=r1[3],font=("",11),bg=notebookcolor,fg="white").place(x=x,y=y)
+        x += 120
+        Label(f3,text=r1[4],font=("",11),bg=notebookcolor,fg="white").place(x=x,y=y)
+        x = 30
+        y+=20
+    db.commit()
+    db.close()
+
+def update(nb):
+    f4=Frame(bg=notebookcolor  )
+    nb.add(f4,text="Update")
+
+def delete(nb):
+    f5=Frame(bg=notebookcolor  )
+    nb.add(f5,text="Delete")
+
+
+def logout(nb):
+    f6=Frame(bg= notebookcolor)
+    nb.add(f6,text="Logout")
+    Button(f6,text="logout",command=home).place(x=int(width)/2,y=int(height)/2)
+
+   
 
 def login():
     db = sqlite3.connect('projectdb.db')
